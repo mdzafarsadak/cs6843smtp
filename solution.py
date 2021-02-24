4d15005296ea425b9ff53ee29aff0a0be60df897
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-23 20:35:47 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,36 +13,36 @@ clientSocket.connect(mailserver)
 
 recv = clientSocket.recv(1024)
 recv = recv.decode()
-print("Message after connection request:" + recv)
+#print("Message after connection request:" + recv)
 if recv[:3] != '220':
-    print('220 reply not received from server.')
+    #print('220 reply not received from server.')
 
 heloCommand = 'HELO Alice\r\n'
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024)
 recv1 = recv1.decode()
-print("Message after EHLO command:" + recv1)
+#print("Message after EHLO command:" + recv1)
 if recv1[:3] != '250':
-    print('250 reply not received from server.')
+    #print('250 reply not received from server.')
 
 
 mailFrom = "MAIL FROM: <bappy1122@gmail.com> \r\n"
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024)
 recv2 = recv2.decode()
-print("After MAIL FROM command: "+recv2)
+#print("After MAIL FROM command: "+recv2)
 
 rcptTo = "RCPT TO: <ms12871@nyu.edu> \r\n"
 clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024)
 recv3 = recv3.decode()
-print("After RCPT TO command: "+recv3)
+#print("After RCPT TO command: "+recv3)
 
 data = "DATA\r\n"
 clientSocket.send(data.encode())
 recv4 = clientSocket.recv(1024)
 recv4 = recv4.decode()
-print("After DATA command: "+recv4)
+#print("After DATA command: "+recv4)
 
 
 #sub = input("Enter the subject : ")
@@ -54,10 +54,11 @@ clientSocket.send(subject.encode())
 clientSocket.send(msg.encode())
 clientSocket.send(endmsg.encode())
 recv_msg = clientSocket.recv(1024)
-print("Response after sending message body:"+recv_msg.decode())
+#print("Response after sending message body:"+recv_msg.decode())
 
 quit = "QUIT\r\n"
 clientSocket.send(quit.encode())
 recv5 = clientSocket.recv(1024)
-print(recv5.decode())
+#print(recv5.decode())
 clientSocket.close()
+
