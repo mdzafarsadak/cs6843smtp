4c67902651319dbf1a7fa31bf49be33f47e2c003
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-25 19:03:09 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,64 +1,44 @@
 from socket import *
+msg = 'I love computer networking'
+endmsg = '\r\n.\r\n'
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = 'smtp.nyu.edu'
+# Create socket called clientSocket and establish a TCP connection with mailserver
 
-#import time
-
-msg = "\r\n I love Computer"
-endmsg = "\r\n.\r\n"
-
-mailserver = ("mail.smtp2go.com", 2525) #Fill in start #Fill in end
-
+#Fill in end
 clientSocket = socket(AF_INET, SOCK_STREAM)
-#clientSocket = ssl.wrap_socket(clientSocket)
-clientSocket.connect(mailserver)
-
+clientSocket.connect((mailserver, 25))
 recv = clientSocket.recv(1024)
-recv = recv.decode()
-#print("Message after connection request:" + recv)
+print(recv)
 if recv[:3] != '220':
-    #print('220 reply not received from server.')
-
+    print('220 reply not received from server.')
+# Send HELO command and print server response.
 heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand.encode())
+clientSocket.send(heloCommand.encode('utf-8'))
 recv1 = clientSocket.recv(1024)
-recv1 = recv1.decode()
-#print("Message after EHLO command:" + recv1)
-if recv1[:3] != '250':
-    #print('250 reply not received from server.')
-
-
-mailFrom = "MAIL FROM: <bappy1122@gmail.com> \r\n"
-clientSocket.send(mailFrom.encode())
+print(recv1)
+#Send MAIL FROM command and print server response.
+print('Sending MAIL FROM Command')
+clientSocket.send(b"MAIL From: bappy1122@gmail.com\r\n")
 recv2 = clientSocket.recv(1024)
-recv2 = recv2.decode()
-#print("After MAIL FROM command: "+recv2)
-
-rcptTo = "RCPT TO: <ms12871@nyu.edu> \r\n"
-clientSocket.send(rcptTo.encode())
-recv3 = clientSocket.recv(1024)
-recv3 = recv3.decode()
-#print("After RCPT TO command: "+recv3)
-
-data = "DATA\r\n"
-clientSocket.send(data.encode())
-recv4 = clientSocket.recv(1024)
-recv4 = recv4.decode()
-#print("After DATA command: "+recv4)
-
-
-#sub = input("Enter the subject : ")
-subject = "Subject: SMTP Client Python Script \r\n\r\n"
-clientSocket.send(subject.encode())
-#date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
-#date = date + "\r\n\r\n"
-#clientSocket.send(date.encode())
-clientSocket.send(msg.encode())
-clientSocket.send(endmsg.encode())
-recv_msg = clientSocket.recv(1024)
-#print("Response after sending message body:"+recv_msg.decode())
-
-quit = "QUIT\r\n"
-clientSocket.send(quit.encode())
-recv5 = clientSocket.recv(1024)
-#print(recv5.decode())
-clientSocket.close()
-
+#Send RCPT TO command and print server response.
+print("Sending RCPT TO Command")
+clientSocket.send(b"RCPT TO: ms12871@nyu.edu\r\n")
+recv2 = clientSocket.recv(1024)
+print(recv2)
+#Send DATA command and print server response.
+print("Sending DATA Command")
+clientSocket.send(b"DATA\r\n")
+recv2 = clientSocket.recv(1024)
+print(recv2)
+#Send Data and print server response.
+print("Sending Data")
+clientSocket.send(b"SUBJECT: SMTP Mail Client Test\n"+msg.encode('utf-8')+b"\n.\n\r\n")
+recv2 = clientSocket.recv(1024)
+print(recv2)
+#Send QUIT and print server response.
+print("Sending QUIT")
+clientSocket.send(b"QUIT\r\n")
+recv2 = clientSocket.recv(1024)
+print(recv2)
+print("Mail Sent")
